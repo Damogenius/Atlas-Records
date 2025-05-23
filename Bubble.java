29cd17f5fe9676eaca382b14530fd825c73f2901
Damogenius <92662058+Damogenius@users.noreply.github.com>
2025-05-23 16:27:40 +0530

    Update Bubble.java

--- Bubble.java
+++ Bubble.java
@@ -2,7 +2,7 @@
 public class Bubble {
     public static void main(String[] args) {
         int[] arr = {3,4,55,2,1,6};
-        System.out.println(arr);
+        System.out.println("Working or Not");
 
     }}
 
