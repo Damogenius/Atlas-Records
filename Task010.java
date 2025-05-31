d517f36f18e3edcbf247eb74330d776a092cb0de
damogenius <damogenius@gmail.com>
2025-06-01 00:14:47 +0530

    Trying this

---
+++ Task010.java
@@ -0,0 +1,16 @@
+public class Task010 {
+    public static void main(String[] args) {
+        int a=4,b=8,c=6;
+        if(a>b && a>c)
+        {
+            System.out.println("A is greater");
+        }
+        else if (b>c && b>a){
+            System.out.println("B is greater");
+        }
+        else{
+            System.out.println("C is greater");
+        }
+    }
+
+}
