b18ee69f974c43da76835b0acbaca7649d8a0176
damogenius <damogenius@gmail.com>
2025-06-01 00:06:40 +0530

    Revert "Pushing"
    This reverts commit 30315c420de39e00ade79462a6bf1c19b1f072d7.

--- Task006.java
+++
@@ -1,7 +0,0 @@
-public class Task006 {
-    public static void main(String[] args) {
-        int a=2, b=3, c = 0;
-        String ouput=(a>b)?"A is greater than B":"B is greater than A";
-        System.out.println(ouput);
-    }
-}
