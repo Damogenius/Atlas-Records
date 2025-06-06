08c048388c683229288f61bfbc5cab39b3038813
damogenius <damogenius@gmail.com>
2025-06-06 18:15:30 +0530

    EOD

---
+++ Task020.java
@@ -0,0 +1,12 @@
+public class Task020 {
+    public static void main(String[] args) {
+        char a[]={'d','a','m','o'};
+        System.out.println(a);
+        int n=a.length;
+        System.out.println("there are "+ n +" letters in my name");
+        for(int i=0;i<n;i++)
+            System.out.println(a[i]+"");
+
+
+    }
+}
