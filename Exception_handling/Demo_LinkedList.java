345749b1702aab15dcef6b462bd31b4ab834596c
damogenius <damogenius@gmail.com>
2025-07-04 09:43:52 +0530

    Merge remote-tracking branch 'main/java' into java

---
+++ Exception_handling/Demo_LinkedList.java
@@ -0,0 +1,57 @@
+package Exception_handling;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+
+public class Demo_LinkedList {
+    Node head;
+
+    // Method to insert node at the end
+    public void insert(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+
+            while (current.next != null) {
+                current = current.next;
+            }
+
+            current.next = newNode;
+        }
+    }
+
+    public void traverse() {
+        Node current = head;
+
+        System.out.print("Linked List: ");
+        while (current != null) {
+            System.out.print(current.data + "-->");
+            current = current.next;
+        }
+        System.out.print("Null");
+    }
+
+
+    public static void main(String[] args) {
+        Demo_LinkedList list = new Demo_LinkedList();
+
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+        list.insert(40);
+
+
+        list.traverse();
+    }
+}
