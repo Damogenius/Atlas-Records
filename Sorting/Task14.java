5c06d917179563f034c409975fa983050cb7faa0
damogenius <damogenius@gmail.com>
2025-07-11 15:33:29 +0530

    Merge Sort

---
+++ Sorting/Task14.java
@@ -0,0 +1,52 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class Task14 {
+
+    public static void mergeSort(int[] arr) {
+        if (arr.length < 2) {
+            return;
+        }
+
+        int mid = arr.length / 2;
+
+        int[] left = Arrays.copyOfRange(arr, 0, mid);
+        int[] right = Arrays.copyOfRange(arr, mid, arr.length);
+
+        mergeSort(left);
+        mergeSort(right);
+
+        merge(arr, left, right);
+    }
+
+    public static void merge(int[] arr, int[] left, int[] right) {
+        int i = 0, j = 0, k = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
+                arr[k++] = left[i++];
+            } else {
+                arr[k++] = right[j++];
+            }
+        }
+
+        while (i < left.length) {
+            arr[k++] = left[i++];
+        }
+
+        while (j < right.length) {
+            arr[k++] = right[j++];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] myList = {38, 27, 43, 3, 9, 82, 10};
+        System.out.println("Original list: " + Arrays.toString(myList));
+
+        mergeSort(myList);
+
+        System.out.println("Sorted list: " + Arrays.toString(myList));
+    }
+}
+
