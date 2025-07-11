701c0171b8ed3300f747ed6c070fec1af25b015d
damogenius <damogenius@gmail.com>
2025-07-11 12:04:08 +0530

    Bubble sort

---
+++ Sorting/Task06.java
@@ -0,0 +1,36 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class Task06 {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < n - 1 - i; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) break;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] myList = {64, 34, 25, 12, 22, 11, 90};
+        System.out.println("Original list: " + Arrays.toString(myList));
+
+        bubbleSort(myList);
+
+        System.out.println("Sorted list: " + Arrays.toString(myList));
+    }
+}
