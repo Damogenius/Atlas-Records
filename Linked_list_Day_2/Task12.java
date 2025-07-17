ec4530867742623921125fc0cbd50380cd24bf75
damogenius <damogenius@gmail.com>
2025-07-17 18:57:49 +0530

    Factorial - Home Task

---
+++ Linked_list_Day_2/Task12.java
@@ -0,0 +1,19 @@
+package Linked_list_Day_2;
+
+public class Task12 {
+
+    // Recursive method to calculate factorial
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int number = 5;
+        int result = factorial(number);
+
+        System.out.println("Factorial of " + number + " is: " + result);
+    }
+}
